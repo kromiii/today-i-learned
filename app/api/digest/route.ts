b22da33820a53c37bb07139bd2d405962ae39ed9
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-03 11:58:17 +0900

    ユーザ認証をチェック

--- app/api/digest/route.ts
+++ app/api/digest/route.ts
@@ -1,7 +1,8 @@
-import { NextResponse } from 'next/server';
-import OpenAI from 'openai';
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
+import { getCurrentUser } from "@/libs/firebase/firebase-admin";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,26 +16,32 @@ const Wisdom = z.object({
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return NextResponse.json({ message: "Not authorized" }, { status: 402 });
+  }
+
   try {
     const completion = await openai.beta.chat.completions.parse({
       model: "gpt-4o-2024-08-06",
       messages: [
-        { role: "system", content: "Extract what the user learned from the chat to the bot" },
-        ...messages
+        {
+          role: "system",
+          content: "Extract what the user learned from the chat to the bot",
+        },
+        ...messages,
       ],
       response_format: zodResponseFormat(Wisdom, "wisdom"),
     });
 
     const wisdom = completion.choices[0].message.parsed;
-
-    // return wisdom as json object
-    // {
-    //   "title": "Embrace Change",
-    //   "description": "The conversation highlighted the importance of adapting to change and learning from new experiences.
-    // }
     return NextResponse.json({ result: wisdom });
   } catch (error) {
-    console.error('Error:', error);
-    return NextResponse.json({ error: 'An error occurred during your request.' }, { status: 500 });
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "An error occurred during your request." },
+      { status: 500 }
+    );
   }
 }
