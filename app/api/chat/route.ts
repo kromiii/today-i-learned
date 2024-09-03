e1cf919e17abed6a61d52a6bd7e78b21b18d6651
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-03 11:58:59 +0900

    ユーザーをチェック

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from 'next/server';
-import OpenAI from 'openai';
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+import { getCurrentUser } from "@/libs/firebase/firebase-admin";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,6 +8,12 @@ const openai = new OpenAI({
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  const user = await getCurrentUser();
+
+  if (!user) {
+    return NextResponse.json({ message: "Not authorized" }, { status: 402 });
+  }
 
   try {
     const completion = await openai.chat.completions.create({
@@ -16,6 +23,9 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ result: completion.choices[0].message });
   } catch (error) {
-    return NextResponse.json({ error: 'An error occurred during your request.' }, { status: 500 });
+    return NextResponse.json(
+      { error: "An error occurred during your request." },
+      { status: 500 }
+    );
   }
 }
