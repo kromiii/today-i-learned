231c8be0db82caea346c6e3fd1f826582b72781e
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-08 12:43:27 +0900

    ハンバーガーメニュー化

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,18 @@
 import { getCurrentUser } from "@/libs/firebase/firebase-admin";
-import SignOutButton from "./header/SignOutButton";
 import Link from "next/link";
+import HamburgerMenu from "@/components/header/HamburgerMenu";
 
 export default async function Header() {
   const currentUser = await getCurrentUser();
 
   return (
     <header className="bg-blue-500 p-4">
-      <nav className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-2xl font-bold">
-          Today I Learned
-        </Link>
-        <div>
-          {currentUser && (
-            <>
-              <Link href="/chat" className="text-white mr-4">
-                Chat
-              </Link>
-              <Link href="/dashboard" className="text-white mr-4">
-                Dashboard
-              </Link>
-              <SignOutButton />
-            </>
-          )}
+      <nav className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          {currentUser && <HamburgerMenu />}
+          <Link href="/" className="text-white text-2xl font-bold ml-4">
+            Today I Learned
+          </Link>
         </div>
       </nav>
     </header>
--- components/header/SignOutButton.tsx
+++ components/header/SignOutButton.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { useRouter } from 'next/navigation';
 import { signOut } from "@/libs/firebase/auth";
 
-export default function SignOutButton() {
+interface SignOutButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  // 追加のプロップがあればここに定義
+}
+
+export default function SignOutButton({ className, ...props }: SignOutButtonProps) {
   const router = useRouter();
 
   const handleSignOut = async () => {
@@ -16,8 +20,9 @@ export default function SignOutButton() {
 
   return (
     <button
+    className={`text-gray-700 hover:text-gray-900 ${className}`}
+    {...props}
       onClick={handleSignOut}
-      className="text-white hover:underline px-4 py-2 transition duration-300 ease-in-out"
     >
       Sign Out
     </button>
