f61c16bbe603611a6c78ae83a9d36b4ce3876fa0
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-10 08:56:19 +0900

    z-index を調整

--- components/header/HamburgerMenu.tsx
+++ components/header/HamburgerMenu.tsx
@@ -53,7 +53,7 @@ export default function HamburgerMenu() {
       </button>
 
       {isOpen && (
-        <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+        <div className="absolute left-0 top-full mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
           <Link
             href="/chat"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
