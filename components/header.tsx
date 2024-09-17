1d1679adc6744d3278161989ce1ea8909cd43433
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-17 20:21:51 +0900

    show github icon in the header

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from "@/libs/firebase/firebase-admin";
 import Link from "next/link";
 import HamburgerMenu from "@/components/header/HamburgerMenu";
+import { FaGithub } from 'react-icons/fa'; // GitHubアイコンをインポート
 
 export default async function Header() {
   const currentUser = await getCurrentUser();
@@ -12,6 +13,16 @@ export default async function Header() {
           {currentUser && <HamburgerMenu />}
           <Link href="/" className="text-white text-2xl font-bold ml-4">
             Today I Learned
+          </Link>
+        </div>
+        <div>
+          <Link 
+            href="https://github.com/kromiii/today-i-learned" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-200"
+          >
+            <FaGithub size={24} />
           </Link>
         </div>
       </nav>
