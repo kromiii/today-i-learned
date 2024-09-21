1a28bdd3fd08e61558c784e8f766fe9c9706300a
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-09-21 21:58:40 +0900

    ヘッダーが端まで広がらないようにする

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -6,8 +6,10 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   return (
-    <header className="bg-white p-4 border-b">
-      {children}
+    <header className="bg-white border-b">
+      <div className="max-w-4xl mx-auto p-4">
+        {children}
+      </div>
     </header>
   );
 }
